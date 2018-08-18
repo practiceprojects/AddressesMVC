9aab9c3f73d9649a164351836248570f5f61a96d
malloryrbush <malloryrbush@hotmail.com>
2018-08-18 18:15:09 -0400

    practiceProject

---
+++ AddressesMVC/Controllers/AddressesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AddressesMVC;
+
+namespace AddressesMVC.Controllers
+{
+    public class AddressesController : Controller
+    {
+        private AddressDBV1Entities db = new AddressDBV1Entities();
+
+        // GET: Addresses
+        public ActionResult Index()
+        {
+            var addresses = db.Addresses.Include(a => a.Customer).Include(a => a.State);
+            return View(addresses.ToList());
+        }
+
+        // GET: Addresses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Address address = db.Addresses.Find(id);
+            if (address == null)
+            {
+                return HttpNotFound();
+            }
+            return View(address);
+        }
+
+        // GET: Addresses/Create
+        public ActionResult Create()
+        {
+            ViewBag.AddressID = new SelectList(db.Customers, "CustomerID", "FirstName");
+            ViewBag.AddressID = new SelectList(db.States, "StateID", "State1");
+            return View();
+        }
+
+        // POST: Addresses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AddressID,StreetNumber,StreetName,CustomerID,StateID")] Address address)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Addresses.Add(address);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AddressID = new SelectList(db.Customers, "CustomerID", "FirstName", address.AddressID);
+            ViewBag.AddressID = new SelectList(db.States, "StateID", "State1", address.AddressID);
+            return View(address);
+        }
+
+        // GET: Addresses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Address address = db.Addresses.Find(id);
+            if (address == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AddressID = new SelectList(db.Customers, "CustomerID", "FirstName", address.AddressID);
+            ViewBag.AddressID = new SelectList(db.States, "StateID", "State1", address.AddressID);
+            return View(address);
+        }
+
+        // POST: Addresses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AddressID,StreetNumber,StreetName,CustomerID,StateID")] Address address)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(address).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AddressID = new SelectList(db.Customers, "CustomerID", "FirstName", address.AddressID);
+            ViewBag.AddressID = new SelectList(db.States, "StateID", "State1", address.AddressID);
+            return View(address);
+        }
+
+        // GET: Addresses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Address address = db.Addresses.Find(id);
+            if (address == null)
+            {
+                return HttpNotFound();
+            }
+            return View(address);
+        }
+
+        // POST: Addresses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Address address = db.Addresses.Find(id);
+            db.Addresses.Remove(address);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AddressesMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AddressesMVC.MvcApplication" Language="C#" %>
